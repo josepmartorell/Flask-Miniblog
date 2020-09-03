a03f702296a4aa0c69315e4901612541d08f35c9
josepmartorell <martorelljosep@gmail.com>
2020-09-03 17:54:50 +0200

    update

--- config/prod.py
+++ config/prod.py
@@ -1,7 +1,9 @@
+import os
+
 from .default import *
 
 
-SECRET_KEY = '5e04a4955d8878191923e86fe6a0dfb24edb226c87d6c7787f35ba4698afc86e95cae409aebd47f7'
+SECRET_KEY = os.urandom(32)
 
 APP_ENV = APP_ENV_PRODUCTION
 
