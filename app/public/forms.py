722b028c65d066834afd67cff70a6e3db1e2f77c
josepmartorell <martorelljosep@gmail.com>
2020-09-09 05:05:09 +0200

    contact form

--- app/public/forms.py
+++ app/public/forms.py
@@ -1,8 +1,16 @@
-from flask_wtf import FlaskForm
-from wtforms import SubmitField, TextAreaField
+from flask_wtf import FlaskForm, Form, validators
+from wtforms import SubmitField, TextAreaField, StringField
 from wtforms.validators import DataRequired
 
 
 class CommentForm(FlaskForm):
     content = TextAreaField('Content', validators=[DataRequired(), ])
     submit = SubmitField('Comment')
+
+
+class ContactForm(Form):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    subject = StringField("Subject", validators=[DataRequired()])
+    message = TextAreaField("Message", validators=[DataRequired()])
+    submit = SubmitField("Send")
